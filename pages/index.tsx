fbf1ae9157c9a1db6b9130ff3116952adb91cc41
xavialfaro <nxb.developer@gmail.com>
2022-10-10 22:38:55 -0500

    add eslint and prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
-import type { NextPage } from 'next';
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return (
-    <h1 className='text-4xl text-orange-500'>
-      Best platform for content creators is comming
-    </h1>
-  );
-};
+  return <h1 className='text-4xl text-orange-500'>Best platform for content creators is comming</h1>
+}
 
-export default Home;
+export default Home
