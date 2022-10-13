d4126c51bd7cfe6bc880da4f3e9aba593fa0cd76
xavialfaro <nxb.developer@gmail.com>
2022-10-13 12:45:39 -0500

    add seo and search page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        title='Content creators'
+        description='Aquí encontrarás a los mejores creadores de contenido que impulsarán tu carrera en tecnología'
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
