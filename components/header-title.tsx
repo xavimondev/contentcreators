059dc38c3bf15224d50a77b2e3c3f80ab6384f69
xavialfaro <nxb.developer@gmail.com>
2022-10-19 12:23:25 -0500

    add fancy text gradient color to title

--- components/header-title.tsx
+++ components/header-title.tsx
@@ -3,7 +3,11 @@ type HeaderProps = {
 }
 
 const HeaderTitle = ({ msg }: HeaderProps) => {
-  return <h1 className='text-center mb-12 text-4xl dark:text-white font-semibold'>{msg}</h1>
+  return (
+    <h1 className='text-center mb-12 text-4xl bg-clip-text text-transparent from-[#2c3037] to-[#ffffff] bg-gradient-to-r font-semibold'>
+      {msg}
+    </h1>
+  )
 }
 
 export default HeaderTitle
