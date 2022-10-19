bd44a6e4c61a7ea07afb49a518a60a99b4c2da94
xavialfaro <nxb.developer@gmail.com>
2022-10-19 17:50:29 -0500

    fix height on layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,11 +3,7 @@ type LayoutProps = {
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  return (
-    <main className='p-6 h-screen bg-gradient-to-l from-[#39497e] to-[#1d184e] w-full'>
-      {children}
-    </main>
-  )
+  return <main className='p-6 bg-gradient-to-l from-[#39497e] to-[#1d184e] w-full'>{children}</main>
 }
 
 export default Layout
