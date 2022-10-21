d21e0cea9d777de26d758e2a1264f8f834d6bd3d
xavialfaro <nxb.developer@gmail.com>
2022-10-20 22:26:27 -0500

    update height on layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,11 @@ type LayoutProps = {
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  return <main className='p-6 bg-gradient-to-l from-[#39497e] to-[#1d184e] w-full'>{children}</main>
+  return (
+    <main className='p-6 min-h-screen h-full bg-gradient-to-l from-[#39497e] to-[#1d184e] w-full'>
+      {children}
+    </main>
+  )
 }
 
 export default Layout
