dda8a6019eff93d729185b674e54c0ed59129e53
xavialfaro <nxb.developer@gmail.com>
2022-10-22 23:45:55 -0500

    add another brakpoint for masonry

--- components/list-creator.tsx
+++ components/list-creator.tsx
@@ -11,7 +11,7 @@ const ListCreator = ({ listCreators }: PropsListCreator) => {
   return (
     <>
       {/* Section */}
-      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 4 }}>
+      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 4, 1200: 5 }}>
         <Masonry gutter='10px'>
           {listCreators.map(({ id, name, description, categories, social }) => (
             <CreatorCard
