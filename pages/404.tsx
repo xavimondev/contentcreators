1142b1cdbb54c97fa35e0fc6bee8a6bc04afb9fb
xavialfaro <nxb.developer@gmail.com>
2022-10-23 18:08:09 -0500

    add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,27 @@
+import CustomLink from 'components/custom-link'
+import Layout from 'components/layout'
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <div className='py-32 max-w-4xl mx-auto'>
+        <div className='flex flex-col gap-8 text-center sm:items-center'>
+          <h1 className='text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#d770b2] to-[#e4ad7a]'>
+            Página no encontrada
+          </h1>
+          <p className='text-xl text-white font-semibold'>
+            Al parecer el acceso a la página ha sido eliminado o nunca existió.
+          </p>
+          <CustomLink
+            href='/'
+            classes='bg-indigo-700 rounded-xl py-4 md:py-3 px-12 text-white font-bold'
+          >
+            Ir a Inicio
+          </CustomLink>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default NotFound
