05a4d1ebf31f5ee91004d8aa59937d12992b398c
Xavier Alfaro <nxb.developer@gmail.com>
2022-12-03 22:16:57 -0500

    update client prisma connection

--- services/client.ts
+++ services/client.ts
@@ -1,20 +1,22 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient
-}
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
 
 export let prisma: PrismaClient
 
 if (typeof window === 'undefined') {
   if (process.env.NODE_ENV === 'production') {
-    global.prisma = new PrismaClient()
+    prisma = new PrismaClient()
   } else {
-    if (!global.prisma) {
-      global.prisma = new PrismaClient()
+    const globalWithPrisma = global as typeof globalThis & {
+      prisma: PrismaClient
     }
-    prisma = global.prisma
+    if (!globalWithPrisma.prisma) {
+      globalWithPrisma.prisma = new PrismaClient()
+    }
+    prisma = globalWithPrisma.prisma
   }
 }
