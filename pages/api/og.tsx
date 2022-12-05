a61fb807e6b564ffa0e12006234fa1b37c3ebac7
Xavier Alfaro <nxb.developer@gmail.com>
2022-12-04 21:31:42 -0500

    add og card profile

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,6 +1,5 @@
 import { ImageResponse } from '@vercel/og'
 import { CREATORS_DATA } from 'data/creators'
-import Image from 'next/image'
 import { NextRequest } from 'next/server'
 
 export const config = {
@@ -12,66 +11,76 @@ export default function handler(req: NextRequest) {
     const { searchParams } = req.nextUrl
 
     const hasUsername = searchParams.has('username')
-    const username = hasUsername ? searchParams.get('username') : 'midudev'
+    const username = hasUsername ? searchParams.get('username') : ''
     const creatorInfo = CREATORS_DATA.find((creator) => creator.id === username)
 
-    console.log(creatorInfo)
-
-    if (!username) {
+    if (!creatorInfo) {
       return new ImageResponse(<>Visit with &quot;?username=vercel&quot;</>, {
         width: 1200,
         height: 630
       })
     }
-
+    console.log(creatorInfo)
+    const { name, description, id } = creatorInfo
     return new ImageResponse(
       (
         <div
           style={{
             display: 'flex',
-            fontSize: 60,
-            color: 'black',
             backgroundImage: 'linear-gradient(to left, #39497e, #1d184e)',
-            width: '100%',
-            height: '100%',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center'
+            height: '100%'
           }}
         >
-          <img
-            width='256'
-            height='256'
-            src={`https://github.com/${username}.png`}
-            style={{
-              borderRadius: 128
-            }}
-          />
           <div
             style={{
               display: 'flex',
-              flexDirection: 'column'
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: 'auto',
+              maxWidth: '40rem',
+              width: '100%'
             }}
           >
-            <h1
+            <img
+              alt={name}
+              width='210'
+              height='210'
+              src={`https://github.com/${id}.png`}
               style={{
-                fontWeight: 700,
-                backgroundImage: 'linear-gradient(to right, #d770b2, #e4ad7a)',
-                backgroundClip: 'text',
-                WebkitBackgroundClip: 'text',
-                color: 'transparent'
+                borderRadius: '50%'
+              }}
+            />
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '20px',
+                marginLeft: '40px'
               }}
             >
-              {creatorInfo?.name}
-            </h1>
-            <p
-              style={{
-                color: 'white',
-                fontSize: '12px'
-              }}
-            >
-              {creatorInfo?.description}
-            </p>
+              <h1
+                style={{
+                  fontSize: '40px',
+                  fontWeight: 'bold',
+                  backgroundImage: 'linear-gradient(to right, #d770b2, #e4ad7a)',
+                  backgroundClip: 'text',
+                  WebkitBackgroundClip: 'text',
+                  color: 'transparent'
+                }}
+              >
+                {name}
+              </h1>
+              <p
+                style={{
+                  color: 'white',
+                  fontSize: '22px',
+                  fontWeight: 'bold'
+                }}
+              >
+                {description}
+              </p>
+            </div>
           </div>
         </div>
       ),
