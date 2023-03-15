50d412c26f86a8feefa268726cc27b7d42c11639
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-15 13:11:33 -0500

    add icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -232,3 +232,21 @@ export const ShareIc = (props: SVGProps<SVGSVGElement>) => {
     </svg>
   )
 }
+
+export const LeftArrowIc = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg viewBox='0 0 21 21' width='21' xmlns='http://www.w3.org/2000/svg' {...props}>
+      <g
+        fill='none'
+        fillRule='evenodd'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        transform='translate(3 6)'
+      >
+        <path d='m4.499.497-3.999 4.002 4 4.001'></path>
+        <path d='m13.5 4.5h-13'></path>
+      </g>
+    </svg>
+  )
+}
