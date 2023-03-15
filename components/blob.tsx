d3921d20162c3bf982d32363ec88ac9b1ef02f39
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-15 18:38:52 -0500

    add new blob

--- components/blob.tsx
+++ components/blob.tsx
@@ -9,4 +9,14 @@ const Blob = () => {
   )
 }
 
+export const BlobListCreators = () => {
+  return (
+    <div className='absolute w-1/2 h-96 top-10 left-1/4 right-1/4 opacity-50 filter blur-[120px] z-0'>
+      <div className='relative'>
+        <div className='absolute top-10 left-24 rounded-full h-96 w-full bg-blue-600'></div>
+        <div className='rounded-full h-96 w-full bg-purple-800'></div>
+      </div>
+    </div>
+  )
+}
 export default Blob
