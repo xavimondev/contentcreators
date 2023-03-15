bfd686442cf4feb197dad1b0bfaebc59b37a23c1
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-15 13:14:25 -0500

    update card creator styles

--- components/creator.tsx
+++ components/creator.tsx
@@ -40,7 +40,7 @@ const CreatorCard = ({ id, name, description, categories, socialLinks }: Creator
 
   return (
     <article
-      className='rounded-2xl border-none dark:bg-slate-900'
+      className='rounded-2xl border-none bg-[#1E1C26]'
       style={{
         opacity: 0,
         transform: 'translateY(100px)'
@@ -69,16 +69,16 @@ const CreatorCard = ({ id, name, description, categories, socialLinks }: Creator
         {/* Description section */}
         <div className='flex flex-col gap-2'>
           {/* Name */}
-          <h2 className='text-lg md:text-3xl font-bold tracking-tight dark:text-white'>{name}</h2>
+          <h2 className='text-lg md:text-2xl font-bold tracking-tight dark:text-white'>{name}</h2>
           {/* Description */}
-          <p className='mb-3 text-base font-normal text-gray-600 dark:text-gray-300 max-w-5xl'>
+          <p className='mb-3 font-normal text-gray-600 dark:text-gray-300 max-w-5xl'>
             {description}
           </p>
           {/* Categories area */}
           <div className='flex flex-row gap-2 flex-wrap w-full'>
             {categories.map((cat) => (
               <CustomLink key={cat} href={`/category/${cat}`}>
-                <span className='bg-blue-100 text-blue-800 text-sm font-medium px-2 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800'>
+                <span className='bg-blue-100 text-blue-800 text-sm font-medium px-2 py-1 rounded dark:bg-purple-500/25 dark:text-purple-300'>
                   {cat}
                 </span>
               </CustomLink>
@@ -87,7 +87,7 @@ const CreatorCard = ({ id, name, description, categories, socialLinks }: Creator
         </div>
         {/* Social section */}
         <div className='flex flex-col gap-2 md:gap-4'>
-          <h5 className='text-base md:text-lg text-white font-semibold'>Sígueme en:</h5>
+          <h5 className='text-base text-white font-semibold'>Sígueme en:</h5>
           <ul className='flex gap-4 flex-wrap'>
             {socialLinks.map(({ id, url }) => (
               <SocialLink key={id} id={id} url={url} />
