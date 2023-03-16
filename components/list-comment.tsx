7c0cfe1019d22a3cc1b8c319e70e54a22bd075ef
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-15 22:05:35 -0500

    remove unnecessary prop

--- components/list-comment.tsx
+++ components/list-comment.tsx
@@ -9,12 +9,8 @@ type ListCommentProps = {
 const ListComment = ({ listComments }: ListCommentProps) => {
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4'>
-      {listComments.map((data: Comment, index: number) => (
-        <CommentCard
-          key={data.id}
-          commentInfo={data}
-          className={`${index === 0 ? 'animate-pulse' : ''}`}
-        />
+      {listComments.map((data: Comment) => (
+        <CommentCard key={data.id} commentInfo={data} />
       ))}
     </div>
   )
