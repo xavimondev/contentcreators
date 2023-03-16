540fc6b10972e48c3c047594ecefd1287f636992
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-15 20:35:31 -0500

    update styles

--- components/toolbar-user.tsx
+++ components/toolbar-user.tsx
@@ -43,41 +43,60 @@ const ToolbarUser = ({
   }, [session])
 
   return (
-    <div className='fixed flex flex-row justify-center gap-1 left-0 right-0 bottom-4 sm:bottom-4 sm:right-4 sm:left-auto rounded-3xl bg-[#1E1C26] w-3/4 m-auto sm:w-60 px-6 py-4'>
-      {userSession ? (
-        <div className='flex flex-row items-center justify-center gap-4 sm:gap-3 w-full'>
-          <div className='relative w-6 h-6'>
-            <Image
-              src={userSession.avatarUrl}
-              className='rounded-full'
-              alt={userSession.fullName}
-              fill
-            />
+    <div
+      className='fixed 
+      flex 
+      flex-row 
+      justify-center 
+      gap-1 left-0 
+      right-0 
+      bottom-4 
+      sm:bottom-4 
+      sm:right-4 
+      sm:left-auto 
+      rounded-3xl 
+      p-0.5
+      bg-gradient-to-r from-indigo-500 to-[#d5578f]
+      w-3/4 
+      m-auto 
+      sm:w-72'
+    >
+      <div className='rounded-3xl bg-[#1E1C26] px-6 py-4 w-full'>
+        {userSession ? (
+          <div className='flex flex-row items-center justify-center gap-4 sm:gap-3 w-full'>
+            <div className='relative w-6 h-6'>
+              <Image
+                src={userSession.avatarUrl}
+                className='rounded-full'
+                alt={userSession.fullName}
+                fill
+              />
+            </div>
+            <span className='text-base font-semibold text-white'>{userSession.username}</span>
+            <div className='flex flex-row gap-4 sm:gap-3'>
+              <button ref={buttonCommentRef} onClick={() => setIsOpen(true)}>
+                <CommentIc className='w-6 h-6 text-white' />
+              </button>
+              <button
+                onClick={() => {
+                  setUserSession(null)
+                  signout()
+                }}
+              >
+                <LogoutIc className='w-6 h-6 text-white' />
+              </button>
+            </div>
           </div>
-          <span className='text-base font-semibold text-white'>{userSession.username}</span>
-          <div className='flex flex-row gap-4 sm:gap-3'>
-            <button ref={buttonCommentRef} onClick={() => setIsOpen(true)}>
-              <CommentIc className='w-6 h-6 text-white' />
-            </button>
-            <button
-              onClick={() => {
-                setUserSession(null)
-                signout()
-              }}
-            >
-              <LogoutIc className='w-6 h-6 text-white' />
-            </button>
-          </div>
-        </div>
-      ) : (
-        <button
-          className='flex flex-row items-center justify-center gap-3 sm:gap-2 w-full'
-          onClick={() => signInWithGitHub(creatorId)}
-        >
-          <span className='text-base font-semibold text-white'>Iniciar Sesión</span>
-          <GitHubIc className='w-6 h-6 text-white' />
-        </button>
-      )}
+        ) : (
+          <button
+            className='flex flex-row items-center justify-center gap-3 sm:gap-2 w-full'
+            onClick={() => signInWithGitHub(creatorId)}
+          >
+            <span className='text-base font-semibold text-white'>Iniciar Sesión</span>
+            <GitHubIc className='w-6 h-6 text-white' />
+          </button>
+        )}
+      </div>
     </div>
   )
 }
