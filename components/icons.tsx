3caffa6fa29577546e6cb8f87b2aba9d34055252
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-16 21:48:08 -0500

    add trash icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -250,3 +250,24 @@ export const LeftArrowIc = (props: SVGProps<SVGSVGElement>) => {
     </svg>
   )
 }
+
+export const TrashIc = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      viewBox='0 0 24 24'
+      strokeWidth='1.8'
+      fill='currentColor'
+      xmlns='http://www.w3.org/2000/svg'
+      color='#000000'
+      {...props}
+    >
+      <path
+        d='M20 9l-1.995 11.346A2 2 0 0116.035 22h-8.07a2 2 0 01-1.97-1.654L4 9M21 6h-5.625M3 6h5.625m0 0V4a2 2 0 012-2h2.75a2 2 0 012 2v2m-6.75 0h6.75'
+        stroke='currentColor'
+        strokeWidth='1.8'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
