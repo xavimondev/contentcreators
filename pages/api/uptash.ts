582aa36a011b8cc4a5e72e5d7c79511da54a0286
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-19 23:10:47 -0500

    update expiration time

--- pages/api/uptash.ts
+++ pages/api/uptash.ts
@@ -1,6 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Redis } from '@upstash/redis'
 
+import { SECONDS_TTL_REDIS } from 'global/constants'
+
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN
@@ -27,7 +29,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { creatorUsername, commentAuthor, commentValue, createdAtMilliseconds } = req.body
   const key = `${creatorUsername}:${commentAuthor}:${createdAtMilliseconds}`
   const result = await redis.set(key, commentValue, {
-    ex: 120
+    ex: SECONDS_TTL_REDIS
   })
   res.status(200).json({ msg: result })
 }
