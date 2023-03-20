282a5d79bfcb39618630537847f9ad359837d337
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-20 11:37:57 -0500

    add stories are fetching from api

--- components/stories-creator.tsx
+++ components/stories-creator.tsx
@@ -12,49 +12,53 @@ const plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ['latin']
 })
 
-const StoryCard = ({ story, config }: { story: Story; config: any }) => {
+export const StoryCard = ({
+  story,
+  config,
+  message,
+  gradient
+}: {
+  story: Story
+  config: any
+  message: string
+  gradient: any
+}) => {
+  const { colorFrom, colorTo } = gradient
   return (
     <WithHeader story={story} globalHeader={config.header}>
-      <div className='bg-gradient-to-r from-cyan-500 to-blue-500 py-28 px-4 w-full h-full flex items-center justify-center'>
-        <p className={`font-semibold text-xl text-white ${plusJakartaSans.className}`}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet, odio eu malesuada
-          commodo, justo ipsum tristique nunc, ac tempor odio augue quis nisl. Duis porttitor quam
-          vel nibh finibus, in malesuada nulla auctor.
-        </p>
+      <div
+        className='py-28 px-4 w-full h-full flex items-center justify-center'
+        style={{
+          background: `linear-gradient(90deg, ${colorFrom}, ${colorTo})`
+        }}
+      >
+        <p className={`font-semibold text-xl text-white ${plusJakartaSans.className}`}>{message}</p>
       </div>
     </WithHeader>
   )
 }
 
-const stories = [
-  {
-    header: {
-      heading: 'Xavier Alfaro',
-      subheading: 'Publicado hace 30min',
-      profileImage: 'https://picsum.photos/100/100'
-    },
-    content: ({ story, config }: { story: Story; config: any }) => (
-      <StoryCard story={story} config={config} />
-    )
-  }
-]
-
 const StoriesCreator = () => {
   const setIsModalStoryOpen = useStore((state) => state.setIsModalStoryOpen)
+  const listStories = useStore((state) => state.listStories)
+  console.log(listStories)
   return (
-    <div className='bg-gradient-to-r from-cyan-500 to-blue-500 w-full h-full relative'>
-      <div className='absolute bottom-4 left-1/2 z-50' onClick={() => setIsModalStoryOpen(false)}>
-        <CancelIc className='text-white h-7 w-7' />
-      </div>
+    <div className='bg-white w-full h-full relative'>
       <Stories
-        stories={stories}
+        stories={listStories}
         defaultInterval={10000}
         width='100%'
-        height='98%'
+        height='100%'
+        keyboardNavigation
+        loop
+        preventDefault
         storyContainerStyles={{
           backgroundColor: 'transparent'
         }}
       />
+      <button className='absolute bottom-4 left-1/2' onClick={() => setIsModalStoryOpen(false)}>
+        <CancelIc className='text-white h-7 w-7' />
+      </button>
     </div>
   )
 }
--- hooks/useStory.tsx
+++ hooks/useStory.tsx
@@ -1,8 +1,16 @@
 import { useEffect } from 'react'
 
+import { CreatorStory } from 'types'
+import type { Story } from 'react-insta-stories/dist/interfaces'
+
 import { useStore } from 'state/store'
 
+import { getRelativeTime } from 'utils/getRelativeTime'
+import { getRandomGradient } from 'utils/getRandomGradient'
+
 import { listCommentsFromCache } from 'services/comment'
+
+import { StoryCard } from 'components/stories-creator'
 
 const useStory = (username: string) => {
   const listStories = useStore((state) => state.listStories)
@@ -12,9 +20,30 @@ const useStory = (username: string) => {
     const getAllStories = async () => {
       try {
         const response = await listCommentsFromCache(username)
-        const data = response.data
+        const data = response.data as CreatorStory[]
         if (data.length === 0) return
-        setListStories(data)
+
+        const stories: Story[] = data.map((creatorStory: CreatorStory, index: number) => {
+          const { author, message, date } = creatorStory
+          const relativeTime = getRelativeTime(new Date(date))
+
+          return {
+            header: {
+              heading: author,
+              subheading: `Publicado ${relativeTime}`,
+              profileImage: `https://avatars.jakerunzer.com/${index}`
+            },
+            content: ({ story, config }: { story: Story; config: any }) => (
+              <StoryCard
+                story={story}
+                config={config}
+                message={message}
+                gradient={getRandomGradient()}
+              />
+            )
+          }
+        })
+        setListStories(stories)
       } catch (error) {
         console.log(error)
       }
