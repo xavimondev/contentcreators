72654f087a35d3b749696b9f263b85eaa7db7727
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-19 21:55:35 -0500

    add state to manage stories

--- state/store.ts
+++ state/store.ts
@@ -1,11 +1,17 @@
 import { create } from 'zustand'
 
+import { CreatorStory } from 'types'
+
 type AppGeneralStore = {
   isModalStoryOpen: boolean
   setIsModalStoryOpen: (isModalOpen: boolean) => void
+  listStories: CreatorStory[]
+  setListStories: (stories: CreatorStory[]) => void
 }
 
 export const useStore = create<AppGeneralStore>()((set) => ({
   isModalStoryOpen: false,
-  setIsModalStoryOpen: (isModalOpen: boolean) => set({ isModalStoryOpen: isModalOpen })
+  setIsModalStoryOpen: (isModalOpen: boolean) => set({ isModalStoryOpen: isModalOpen }),
+  listStories: [],
+  setListStories: (stories: CreatorStory[]) => set({ listStories: stories })
 }))
