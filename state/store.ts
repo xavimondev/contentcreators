d86c99100f94e961fbb9a929e343d7570eae7665
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-20 19:14:27 -0500

    add state to track the index of last story seen

--- state/store.ts
+++ state/store.ts
@@ -7,11 +7,15 @@ type AppGeneralStore = {
   setIsModalStoryOpen: (isModalOpen: boolean) => void
   listStories: Story[]
   setListStories: (stories: Story[]) => void
+  lastStoryIndex: number
+  setLastStoryIndex: (lastStoryIndex: number) => void
 }
 
 export const useStore = create<AppGeneralStore>()((set) => ({
   isModalStoryOpen: false,
   setIsModalStoryOpen: (isModalOpen: boolean) => set({ isModalStoryOpen: isModalOpen }),
   listStories: [],
-  setListStories: (stories: Story[]) => set({ listStories: stories })
+  setListStories: (stories: Story[]) => set({ listStories: stories }),
+  lastStoryIndex: 0,
+  setLastStoryIndex: (lastStoryIndex: number) => set({ lastStoryIndex })
 }))
