908df2cd6b50a4ff3545a15be34d40192a84f546
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-25 11:21:12 -0500

    add domain

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['unavatar.io', 'avatars.githubusercontent.com']
+    domains: ['unavatar.io', 'avatars.githubusercontent.com', 'static-cdn.jtvnw.net']
   },
   i18n: {
     locales: ['es'],
