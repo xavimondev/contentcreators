c6f84d96829770b6566fda65b58989b653f10839
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-30 11:20:18 -0500

    add endpoint to delete story from uptash

--- pages/api/uptash/[id].ts
+++ pages/api/uptash/[id].ts
@@ -7,20 +7,23 @@ const redis = new Redis({
 })
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
-  if (req.method === 'PATCH') {
-    const { id } = req.query
-    // Getting keys by pattern
-    const keys = await redis.keys(`*:${id}:*`)
-    // Just want the first key
-    const key = keys.at(0)
-    if (keys.length === 0 || key == null) return res.status(200).json({ msg: 'Data not found' })
+  const { id } = req.query
+  // Getting keys by pattern
+  const keys = await redis.keys(`*:${id}:*`)
+  // Just want the first key
+  const key = keys.at(0)
+  if (keys.length === 0 || key == null) return res.status(200).json({ msg: 'Data not found' })
 
+  if (req.method === 'PATCH') {
     const { commentValue } = req.body
     // Getting the current TTL and then updating key's value
     const ttl = await redis.ttl(key)
     const result = await redis.set(key, commentValue, {
       ex: ttl
     })
+    return res.status(200).json({ msg: result })
+  } else if (req.method === 'DELETE') {
+    const result = await redis.del(key)
     return res.status(200).json({ msg: result })
   }
 }
