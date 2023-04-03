cd97519947973a9d375ed86d84d981ce6c70236f
Xavier Alfaro <nxb.developer@gmail.com>
2023-04-03 12:17:02 -0500

    remove animations on creator card

--- components/creator.tsx
+++ components/creator.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import { useEffect, useRef, useState } from 'react'
 import type { Category, Social } from 'types'
 import CustomLink from './custom-link'
 import { ShareIc } from './icons'
@@ -13,9 +12,6 @@ type CreatorCardProps = {
   socialLinks: Social[]
   profileUrl: string
 }
-const options = {
-  threshold: 0.1
-}
 
 const CreatorCard = ({
   id,
@@ -25,50 +21,13 @@ const CreatorCard = ({
   socialLinks,
   profileUrl
 }: CreatorCardProps) => {
-  const cardRef = useRef<HTMLDivElement>(null)
-  // I took blur workaround from https://github.com/vercel/next.js/discussions/26168
-  const [isLoading, setLoading] = useState(true)
-
-  // TODO: Convert observer into custom hook
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      const entry = entries[0]
-      if (entry.isIntersecting) {
-        observer.unobserve(entry.target)
-        entry.target.setAttribute('style', 'opacity: 1; transform: translateY(0); transition: 0.4s')
-      }
-    }, options)
-
-    const element = cardRef?.current
-
-    if (element) observer.observe(element)
-
-    return () => observer && observer.disconnect()
-  }, [])
-
   return (
-    <article
-      className='rounded-2xl border-none bg-[#1E1C26]'
-      style={{
-        opacity: 0,
-        transform: 'translateY(100px)'
-      }}
-      ref={cardRef}
-    >
+    <article className='rounded-2xl border-none bg-[#1E1C26] z-10'>
       <div className='flex flex-col gap-4 p-6'>
         {/* Photo section */}
         <div className='flex flex-row justify-between'>
-          <div className='object-cover w-24 md:w-32 h-auto rounded-xl'>
-            <Image
-              className={`rounded-xl duration-700 ease-in-out ${
-                isLoading ? 'grayscale blur-2xl scale-110' : 'grayscale-0 blur-0 scale-100'
-              }`}
-              src={profileUrl}
-              width='256'
-              height='256'
-              alt={name}
-              onLoadingComplete={() => setLoading(false)}
-            />
+          <div className='object-cover w-24 md:w-32 h-auto'>
+            <Image className='rounded-xl' src={profileUrl} width='256' height='256' alt={name} />
           </div>
           <CustomLink href={`/creator/${id}`}>
             <ShareIc className='text-white' />
