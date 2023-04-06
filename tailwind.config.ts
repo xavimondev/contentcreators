891d865791df6ae4c47067ddb1caecf788e4060a
Xavier Alfaro <nxb.developer@gmail.com>
2023-04-05 22:57:12 -0500

    update tailwind file to support src dir

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       animation: {
