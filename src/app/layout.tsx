c29501f1bfe896b34356817cdc9d06a2d4e31bb2
Xavier Alfaro <nxb.developer@gmail.com>
2023-04-09 17:06:39 -0500

    add metadataBase to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import 'styles/globals.css'
 import { Analytics } from '@vercel/analytics/react'
 import type { Metadata } from 'next'
+import { AUTH_REDIRECT } from 'global/constants'
 
 export const metadata: Metadata = {
+  metadataBase: new URL(AUTH_REDIRECT),
   title: 'content.[creators]',
   description:
     'Aquí encontrarás a los mejores creadores de contenido que impulsarán tu carrera en tecnología.',
