e4ac538279d6187e8a873a7e6a3370829081a867
Xavier Alfaro <nxb.developer@gmail.com>
2023-04-09 21:05:56 -0500

    add animation when toast enters and leaves

--- src/components/custom-toast.tsx
+++ src/components/custom-toast.tsx
@@ -7,8 +7,14 @@ type ConfirmToastProps = {
 
 export const ConfirmToast = ({ customToast, acceptFunction }: ConfirmToastProps) => {
   return (
-    <div className='flex flex-col gap-1 bg-white p-4 shadow-md rounded-md'>
-      <span>🤔 ¿Seguro que deseas eliminar tu comentario?</span>
+    <div
+      className={`flex flex-col gap-1 bg-white p-4 shadow-md rounded-md ${
+        customToast.visible ? 'animate-enter' : 'animate-leave'
+      }`}
+    >
+      <span className='text-sm sm:text-base font-semibold mb-2'>
+        🤔 ¿Seguro que deseas eliminar tu comentario?
+      </span>
       <div className='flex gap-2 justify-center'>
         <button
           className='p-2 rounded-md font-medium text-sm text-white bg-indigo-700'
@@ -18,7 +24,7 @@ export const ConfirmToast = ({ customToast, acceptFunction }: ConfirmToastProps)
         </button>
         <button
           className='p-2 rounded-md border font-medium text-sm'
-          onClick={() => toast.remove(customToast.id)}
+          onClick={() => toast.dismiss(customToast.id)}
         >
           Declinar
         </button>
