9f65b338063b0aa20dfc00be5aba2e39024f53a5
Xavier Alfaro <nxb.developer@gmail.com>
2023-04-11 20:04:23 -0500

    add constant

--- global/constants.ts
+++ global/constants.ts
@@ -1,2 +1,6 @@
 export const URL_TWITCH_GENERATE_TOKEN = 'https://id.twitch.tv/oauth2/token'
 export const URL_TWITCH_LIST_STREAMS = 'https://api.twitch.tv/helix/streams'
+export const AUTH_REDIRECT =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'https://contentcreators.vercel.app'
