4d65d6d2724f8696e8f5a0518b1ed26728aa648a
Xavier Alfaro <nxb.developer@gmail.com>
2023-04-12 19:26:18 -0500

    remove unnecessary component

--- src/components/layout.tsx
+++
@@ -1,9 +0,0 @@
-type LayoutProps = {
-  children: React.ReactNode
-}
-
-const Layout = ({ children }: LayoutProps) => {
-  return <main className='p-6 min-h-screen h-full w-full'>{children}</main>
-}
-
-export default Layout
